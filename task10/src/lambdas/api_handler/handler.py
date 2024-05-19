3a6fde6e5aec2e747bcb009534451f3ef94b4a96
Sergei Smolov <sergei_smolov@epam.com>
2024-05-19 23:31:53 +0400

    task10: add business logic and fixes

--- task10/src/lambdas/api_handler/handler.py
+++ task10/src/lambdas/api_handler/handler.py
@@ -1,5 +1,8 @@
+import json
 import os
+from uuid import uuid4
 
+import boto3
 from commons.log_helper import get_logger
 from commons.abstract_lambda import AbstractLambda
 
@@ -17,13 +20,77 @@ class ApiHandler(AbstractLambda):
         """
         # todo implement business logic
         _LOG.info(f"{event=}")
-        reservations_table = os.environ['RESERVATIONS_TABLE']
-        tables_table = os.environ['TABLES_TABLE']
+        reservations_table_name = os.environ['RESERVATIONS_TABLE']
+        tables_table_name = os.environ['TABLES_TABLE']
         user_pool_name = os.environ['USER_POOL']
-        _LOG.info(f"{reservations_table=}")
-        _LOG.info(f"{tables_table=}")
+        _LOG.info(f"{reservations_table_name=}")
+        _LOG.info(f"{tables_table_name=}")
         _LOG.info(f"{user_pool_name=}")
-        return {"event": event}
+
+        dynamodb = boto3.resource('dynamodb')
+        tables_table = dynamodb.Table(tables_table_name)
+        reservations_table = dynamodb.Table(reservations_table_name)
+        try:
+            if event['path'] == '/signup' and event['httpMethod'] == 'POST':
+                _LOG.info("signup post")
+
+            elif event['path'] == '/signin' and event['httpMethod'] == 'POST':
+                _LOG.info("signip post")
+
+            elif event['path'] == '/tables' and event['httpMethod'] == 'POST':
+                _LOG.info("tables post")
+                item = json.loads(event['body'])
+                response = tables_table.put_item(Item=item)
+                _LOG.info(response)
+                return {"statusCode": 200, 
+                        "body": json.dumps({"id": item["id"]})
+                        }
+
+            elif event['path'] == '/tables' and event['httpMethod'] == 'GET':
+                response = tables_table.scan()
+                items = response['Items']
+                _LOG.info(items)
+                return {"statusCode": 200, "body": json.dumps(items)}
+
+            elif event['resource'] == '/tables/{tableId}' and event['httpMethod'] == 'GET':
+                table_id = int(event['path'].split('/')[-1])
+                _LOG.info(f"{table_id=}")
+                
+            elif event['path'] == '/reservations' and event['httpMethod'] == 'POST':
+                _LOG.info("reservations post")
+                item = json.loads(event['body'])
+                reservation_id = uuid4()
+                response = reservations_table.put_item(Item={"reservationId": reservation_id, **item})
+                _LOG.info(response)
+                return {"statusCode": 200, 
+                        "body": json.dumps({"reservationId": reservation_id})
+                        }
+
+            elif event['path'] == '/reservations' and event['httpMethod'] == 'GET':
+                _LOG.info("reservations get")
+                response = reservations_table.scan()
+                items = response['Items']
+                _LOG.info(items)
+                return {"statusCode": 200, "body": json.dumps(items)}
+
+            else:
+                raise KeyError("no method")
+        except Exception as e:
+            _LOG.info('Bad request')
+            _LOG.info(f'Error: {e}')
+
+            message = {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'statusCode': 400,
+                    'error': 'Bad request',
+                    'message': f'{e}'
+                })
+            }
+
+            _LOG.info(f'{message=}')
+            return message
+        return {"statusCode": 200, "event": event}
     
 
 HANDLER = ApiHandler()
